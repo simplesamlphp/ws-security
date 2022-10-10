8961b5137c75a2340e128cd50c89617d4fe431c1
Tim van Dijen <tvdijen@gmail.com>
2022-10-10 23:28:31 +0200

    Add custom exceptions to the library

---
+++ src/Exception/ProtocolViolationException.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\WSSecurity\Exception;
+
+/**
+ * This exception may be raised when a violation of the WS specification is detected
+ *
+ * @package tvdijen/ws-security
+ */
+class ProtocolViolationException extends RuntimeException
+{
+}
