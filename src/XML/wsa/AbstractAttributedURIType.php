0529e6afde44cce0035fd95fb06bde8c07d5baac
Tim van Dijen <tvdijen@gmail.com>
2022-10-10 22:08:44 +0200

    Migrate fromXML signature

--- src/XML/wsa/AbstractAttributedURIType.php
+++ src/XML/wsa/AbstractAttributedURIType.php
@@ -66,12 +66,12 @@ abstract class AbstractAttributedURIType extends AbstractWsaElement
      * Convert XML into a class instance
      *
      * @param \DOMElement $xml The XML element we should load
-     * @return self
+     * @return static
      *
      * @throws \SimpleSAML\XML\Exception\InvalidDOMElementException
      *   If the qualified name of the supplied element is wrong
      */
-    public static function fromXML(DOMElement $xml): object
+    public static function fromXML(DOMElement $xml): static
     {
         Assert::same($xml->localName, static::getLocalName(), InvalidDOMElementException::class);
         Assert::same($xml->namespaceURI, static::NS, InvalidDOMElementException::class);
--- src/XML/wsa/AbstractEndpointReferenceType.php
+++ src/XML/wsa/AbstractEndpointReferenceType.php
@@ -162,7 +162,7 @@ abstract class AbstractEndpointReferenceType extends AbstractWsaElement
      * @throws \SimpleSAML\XML\Exception\InvalidDOMElementException if the qualified name of the supplied element is wrong
      * @throws \SimpleSAML\XML\Exception\MissingAttributeException if the supplied element is missing any of the mandatory attributes
      */
-    public static function fromXML(DOMElement $xml): object
+    public static function fromXML(DOMElement $xml): static
     {
         $qualifiedName = static::getClassName(static::class);
         Assert::eq(
