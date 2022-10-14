66799772a99c0e40001f2d0f0a45c90058b26fc6
Tim van Dijen <tvdijen@gmail.com>
2022-10-14 23:20:12 +0200

    Add wst:InteractiveChallengeResponse

---
+++ src/XML/wst/InteractiveChallengeResponse.php
@@ -0,0 +1,145 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\WSSecurity\XML\wst;
+
+use DOMElement;
+use SimpleSAML\Assert\Assert;
+use SimpleSAML\WSSecurity\Constants as C;
+use SimpleSAML\XML\Chunk;
+use SimpleSAML\XML\Exception\InvalidDOMElementException;
+use SimpleSAML\XML\Exception\SchemaViolationException;
+use SimpleSAML\XML\ExtendableAttributesTrait;
+use SimpleSAML\XML\ExtendableElementTrait;
+
+use function array_filter;
+use function array_values;
+
+/**
+ * @package tvdijen/ws-security
+ */
+final class InteractiveChallengeResponse extends AbstractWstElement
+{
+    use ExtendableAttributesTrait;
+    use ExtendableElementTrait;
+
+    /** @var string|array */
+    public const NAMESPACE = C::XS_ANY_NS_ANY;
+
+
+    /**
+     * Initialize a wst:InteractiveChallengeResponse
+     *
+     * @param \SimpleSAML\XML\ElementInterface[] $elements
+     * @param \DOMAttr[] $namespacedAttributes
+     */
+    public function __construct(array $elements, array $namespacedAttributes = [])
+    {
+        $this->setElements($elements);
+        $this->setAttributesNS($namespacedAttributes);
+    }
+
+
+    /**
+     * @return \SimpleSAML\WSSecurity\XML\wst\TextChallengeResponse[]
+     */
+    public function getTextChallengeResponse(): array
+    {
+        return array_values(array_filter($this->elements, function ($element) {
+            return $element instanceof TextChallengeResponse;
+        }));
+    }
+
+
+    /**
+     * @return \SimpleSAML\WSSecurity\XML\wst\ChoiceChallengeResponse[]
+     */
+    public function getChoiceChallengeResponse(): array
+    {
+        return array_values(array_filter($this->elements, function ($element) {
+            return $element instanceof ChoiceChallengeResponse;
+        }));
+    }
+
+
+    /**
+     * @return \SimpleSAML\WSSecurity\XML\wst\ContextData[]
+     */
+    public function getContextData(): array
+    {
+        return array_values(array_filter($this->elements, function ($element) {
+            return $element instanceof ContextData;
+        }));
+    }
+
+
+    /**
+     * Test if an object, at the state it's in, would produce an empty XML-element
+     *
+     * @return bool
+     */
+    public function isEmptyElement(): bool
+    {
+        return is_null($this->elements) && empty($this->namespacedAttributes);
+    }
+
+
+    /**
+     * Convert XML into a wst:InteractiveChallengeResponse
+     *
+     * @param \DOMElement $xml The XML element we should load
+     * @return static
+     *
+     * @throws \SimpleSAML\XML\Exception\InvalidDOMElementException
+     *   If the qualified name of the supplied element is wrong
+     */
+    public static function fromXML(DOMElement $xml): static
+    {
+        Assert::same($xml->localName, 'InteractiveChallengeResponse', InvalidDOMElementException::class);
+        Assert::same($xml->namespaceURI, InteractiveChallengeResponse::NS, InvalidDOMElementException::class);
+
+        $children = [];
+        foreach ($xml->childNodes as $node) {
+            if (!($node instanceof DOMElement)) {
+                continue;
+            } elseif ($node->namespaceURI === C::NS_TRUST) {
+                $children[] = match ($node->localName) {
+                    'TextChallengeResponse' => TextChallengeResponse::fromXML($node),
+                    'ChoiceChallengeResponse' => ChoiceChallengeResponse::fromXML($node),
+                    'ContextData' => ContextData::fromXML($node),
+                    default => new Chunk($node),
+                };
+            } else {
+                $children[] = new Chunk($node);
+            }
+        }
+
+        return new static($children, self::getAttributesNSFromXML($xml));
+    }
+
+
+    /**
+     * Convert this element to XML.
+     *
+     * @param \DOMElement|null $parent The element we should append this element to.
+     * @return \DOMElement
+     */
+    public function toXML(DOMElement $parent = null): DOMElement
+    {
+        $e = $this->instantiateParentElement($parent);
+
+        foreach ($this->getAttributesNS() as $attr) {
+            $e->setAttributeNS($attr['namespaceURI'], $attr['qualifiedName'], $attr['value']);
+        }
+
+        /** @psalm-var \SimpleSAML\XML\SerializableElementInterface $elt */
+        foreach ($this->getElements() as $elt) {
+            if (!$elt->isEmptyElement()) {
+                $elt->toXML($e);
+            }
+        }
+
+        return $e;
+    }
+}
---
+++ tests/WSSecurity/XML/wst/InteractiveChallengeResponseTest.php
@@ -0,0 +1,97 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\Test\WSSecurity\XML\wst;
+
+use DOMDocument;
+use PHPUnit\Framework\TestCase;
+use SimpleSAML\Assert\AssertionFailedException;
+use SimpleSAML\Test\XML\SchemaValidationTestTrait;
+use SimpleSAML\Test\XML\SerializableElementTestTrait;
+use SimpleSAML\WSSecurity\Constants as C;
+use SimpleSAML\WSSecurity\XML\wst\ChoiceChallengeResponse;
+use SimpleSAML\WSSecurity\XML\wst\ChoiceSelected;
+use SimpleSAML\WSSecurity\XML\wst\ContextData;
+use SimpleSAML\WSSecurity\XML\wst\InteractiveChallengeResponse;
+use SimpleSAML\WSSecurity\XML\wst\TextChallengeResponse;
+use SimpleSAML\XML\Chunk;
+use SimpleSAML\XML\DOMDocumentFactory;
+
+use function dirname;
+use function strval;
+
+/**
+ * Tests for wst:InteractiveChallengeResponse.
+ *
+ * @covers \SimpleSAML\WSSecurity\XML\wst\InteractiveChallengeResponse
+ * @covers \SimpleSAML\WSSecurity\XML\wst\AbstractWstElement
+ * @package tvdijen/ws-security
+ */
+final class InteractiveChallengeResponseTest extends TestCase
+{
+    use SchemaValidationTestTrait;
+    use SerializableElementTestTrait;
+
+
+    /**
+     */
+    protected function setUp(): void
+    {
+        $this->testedClass = InteractiveChallengeResponse::class;
+
+        $this->schema = dirname(dirname(dirname(dirname(dirname(__FILE__))))) . '/schemas/ws-trust.xsd';
+
+        $this->xmlRepresentation = DOMDocumentFactory::fromFile(
+            dirname(dirname(dirname(dirname(__FILE__)))) . '/resources/xml/wst_InteractiveChallengeResponse.xml'
+        );
+    }
+
+
+    // test marshalling
+
+
+    /**
+     * Test creating an InteractiveChallengeResponse object from scratch.
+     */
+    public function testMarshalling(): void
+    {
+        $domAttr = $this->xmlRepresentation->createAttributeNS('urn:x-simplesamlphp:namespace', 'test:attr1');
+        $domAttr->value = 'testval1';
+
+        $textChallengeResponse = new TextChallengeResponse('654321', C::WST_REFID_PIN, [$domAttr]);
+        $choiceChallengeResponse = new ChoiceChallengeResponse(
+            'urn:x-simplesamlphp:namespace',
+            [new ChoiceSelected('urn:x-simplesamlphp:namespace')],
+        );
+        $chunk = new Chunk(DOMDocumentFactory::fromString(
+            '<ssp:Chunk xmlns:ssp="urn:x-simplesamlphp:namespace">Some</ssp:Chunk>'
+        )->documentElement);
+        $contextData = new ContextData('urn:x-simplesamlphp:namespace', [$chunk]);
+        $interactiveChallengeResponse = new InteractiveChallengeResponse(
+            [$textChallengeResponse, $choiceChallengeResponse, $contextData, $chunk],
+            [$domAttr],
+        );
+
+        $this->assertEquals(
+            $this->xmlRepresentation->saveXML($this->xmlRepresentation->documentElement),
+            strval($interactiveChallengeResponse)
+        );
+    }
+
+
+    // test unmarshalling
+
+
+    /**
+     * Test creating a TextChallengeResponse from XML.
+     */
+    public function testUnmarshalling(): void
+    {
+        $interactiveChallengeResponse = InteractiveChallengeResponse::fromXML($this->xmlRepresentation->documentElement);
+        $this->assertEquals(
+            $this->xmlRepresentation->saveXML($this->xmlRepresentation->documentElement),
+            strval($interactiveChallengeResponse)
+        );
+    }
+}
