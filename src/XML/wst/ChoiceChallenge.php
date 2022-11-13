a3aad106888fd069fa40acf3c1713e0fea8de406
Tim van Dijen <tvdijen@gmail.com>
2022-11-14 00:11:53 +0100

    Fix deprecated set-output

---
+++ src/XML/wst/ChoiceChallenge.php
@@ -0,0 +1,178 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\WSSecurity\XML\wst;
+
+use DOMElement;
+use SimpleSAML\Assert\Assert;
+use SimpleSAML\XML\Exception\InvalidDOMElementException;
+use SimpleSAML\XML\Exception\SchemaViolationException;
+use SimpleSAML\XML\Exception\TooManyElementsException;
+use SimpleSAML\XML\ExtendableAttributesTrait;
+use SimpleSAML\WSSecurity\XML\ReferenceIdentifierTrait;
+
+use function array_pop;
+
+/**
+ * @package tvdijen/ws-security
+ */
+final class ChoiceChallenge extends AbstractWstElement
+{
+    use ExtendableAttributesTrait;
+    use ReferenceIdentifierTrait;
+
+    /** @var string|null */
+    protected ?string $label;
+
+    /** @var bool */
+    protected bool $exactlyOne;
+
+    /** @var \SimpleSAML\WSSecurity\XML\wst\Choice|null */
+    protected ?Choice $choice;
+
+
+    /**
+     * Initialize a wst:ChoiceChallenge
+     *
+     * @param string $refId
+     * @param bool $exactlyOne
+     * @param string|null $label
+     * @param \SimpleSAML\WSSecurity\XML\wst\Choice|null $choice
+     * @param \DOMAttr[] $namespacedAttributes
+     */
+    public function __construct(
+        string $refId,
+        bool $exactlyOne,
+        ?string $label = null,
+        ?Choice $choice = null,
+        array $namespacedAttributes = []
+    ) {
+        $this->setRefId($refId);
+        $this->exactlyOne($exactlyOne);
+        $this->setLabel($label);
+        $this->setChoice($choice);
+        $this->setAttributesNS($namespacedAttributes);
+    }
+
+
+    /**
+     * Collect the value of the exactlyOne property.
+     *
+     * @return bool
+     */
+    public function getExactlyOne(): bool
+    {
+        return $this->exactlyOne;
+    }
+
+
+    /**
+     * Set the value of the exactlyOne property.
+     *
+     * @param bool $exactlyOne
+     */
+    protected function setExactlyOne(bool $exactlyOne): void
+    {
+        $this->exactlyOne = $exactlyOne;
+    }
+
+
+    /**
+     * Collect the value of the label property.
+     *
+     * @return string|null
+     */
+    public function getLabel(): ?string
+    {
+        return $this->label;
+    }
+
+
+    /**
+     * Set the value of the label property.
+     *
+     * @param string|null $label
+     */
+    protected function setLabel(?string $label): void
+    {
+        $this->label = $label;
+    }
+
+
+    /**
+     * Collect the value of the choice property.
+     *
+     * @return \SimpleSAML\WSSecurity\XML\wst\Choice|null
+     */
+    public function getChoice(): ?Choice
+    {
+        return $this->choice;
+    }
+
+
+    /**
+     * Set the value of the choice property.
+     *
+     * @param \SimpleSAML\WSSecurity\XML\wst\Choice|null $choice
+     */
+    protected function setChoice(?Choice $choice): void
+    {
+        $this->choice = $choice;
+    }
+
+
+    /**
+     * Convert XML into a wst:ChoiceChallenge
+     *
+     * @param \DOMElement $xml The XML element we should load
+     * @return static
+     *
+     * @throws \SimpleSAML\XML\Exception\InvalidDOMElementException
+     *   If the qualified name of the supplied element is wrong
+     */
+    public static function fromXML(DOMElement $xml): static
+    {
+        Assert::same($xml->localName, 'ChoiceChallenge', InvalidDOMElementException::class);
+        Assert::same($xml->namespaceURI, ChoiceChallenge::NS, InvalidDOMElementException::class);
+
+        /** @psalm-var string $refId */
+        $refId = self::getAttribute($xml, 'RefId');
+        /** @psalm-var bool $exactlyOne */
+        $exactlyOne = self::getBooleanAttribute($xml, 'ExactlyOne');
+        $label = self::getAttribute($xml, 'Label', null);
+
+        $choice = Choice::getChildrenOfClass($xml);
+        Assert::maxCount($choice, 1, TooManyElementsException::class);
+
+        return new static($refId, $label, array_pop($choice), self::getAttributesNSFromXML($xml));
+    }
+
+
+    /**
+     * Convert this element to XML.
+     *
+     * @param \DOMElement|null $parent The element we should append this element to.
+     * @return \DOMElement
+     */
+    public function toXML(DOMElement $parent = null): DOMElement
+    {
+        $e = $this->instantiateParentElement($parent);
+        $e->setAttribute('RefId', $this->refId);
+        $e->setAttribute('ExactlyOne', $this->exactlyOne);
+
+        if ($this->label !== null) {
+            $e->setAttribute('Label', $this->label);
+        }
+
+        if ($this->choice !== null) {
+            $this->choice->toXML($e);
+        }
+
+        foreach ($this->getAttributesNS() as $attr) {
+            $e->setAttributeNS($attr['namespaceURI'], $attr['qualifiedName'], $attr['value']);
+        }
+
+        return $e;
+    }
+}
