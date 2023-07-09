d2cebbd30e81d70de0c627c14d441b9b536b0355
Tim van Dijen <tvdijen@gmail.com>
2023-07-09 23:41:34 +0200

    Unit-test wsse:Nonce

---
+++ tests/WSSecurity/XML/wsse/NonceTest.php
@@ -0,0 +1,87 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\Test\WSSecurity\XML\wsse;
+
+use DOMDocument;
+use PHPUnit\Framework\TestCase;
+use SimpleSAML\WSSecurity\Constants as C;
+use SimpleSAML\WSSecurity\XML\wsse\Nonce;
+use SimpleSAML\XML\Attribute as XMLAttribute;
+use SimpleSAML\XML\DOMDocumentFactory;
+use SimpleSAML\XML\TestUtils\SchemaValidationTestTrait;
+use SimpleSAML\XML\TestUtils\SerializableElementTestTrait;
+
+use function dirname;
+use function strval;
+
+/**
+ * Tests for wsse:Nonce.
+ *
+ * @covers \SimpleSAML\WSSecurity\XML\wsse\Nonce
+ * @covers \SimpleSAML\WSSecurity\XML\wsse\AbstractEncodedString
+ * @covers \SimpleSAML\WSSecurity\XML\wsse\AbstractAttributedString
+ * @covers \SimpleSAML\WSSecurity\XML\wsse\AbstractWsseElement
+ * @package tvdijen/ws-security
+ */
+final class NonceTest extends TestCase
+{
+    use SchemaValidationTestTrait;
+    use SerializableElementTestTrait;
+
+
+    /**
+     */
+    public static function setUpBeforeClass(): void
+    {
+        self::$testedClass = Nonce::class;
+
+        self::$schemaFile = dirname(__FILE__, 5) . '/resources/schemas/oasis-200401-wss-wssecurity-secext-1.0.xsd';
+
+        self::$xmlRepresentation = DOMDocumentFactory::fromFile(
+            dirname(__FILE__, 4) . '/resources/xml/wsse_Nonce.xml',
+        );
+    }
+
+
+    // test marshalling
+
+
+    /**
+     * Test creating a Nonce object from scratch.
+     */
+    public function testMarshalling(): void
+    {
+        $content = 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=';
+        $attr1 = new XMLAttribute('urn:x-simplesamlphp:namespace', 'ssp', 'attr1', 'testval1');
+
+        $nonce = new Nonce(
+            $content,
+            'SomeID',
+            'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd#base64binary',
+            [$attr1],
+        );
+
+        $this->assertEquals(
+            self::$xmlRepresentation->saveXML(self::$xmlRepresentation->documentElement),
+            strval($nonce),
+        );
+    }
+
+
+    // test unmarshalling
+
+
+    /**
+     * Test creating a BinarySecurityToken from XML.
+     */
+    public function testUnmarshalling(): void
+    {
+        $nonce = Nonce::fromXML(self::$xmlRepresentation->documentElement);
+        $this->assertEquals(
+            self::$xmlRepresentation->saveXML(self::$xmlRepresentation->documentElement),
+            strval($nonce),
+        );
+    }
+}
