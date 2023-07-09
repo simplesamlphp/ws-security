243da6b162102db221cf1f19538a398dc57d4618
Tim van Dijen <tvdijen@gmail.com>
2023-07-09 16:59:57 +0200

    Add wsse:TransformationParametersType

---
+++ src/XML/wsse/AbstractTransformationParametersType.php
@@ -0,0 +1,114 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\WSSecurity\XML\wsse;
+
+use DOMElement;
+use SimpleSAML\Assert\Assert;
+use SimpleSAML\WSSecurity\Constants as C;
+use SimpleSAML\XML\Chunk;
+use SimpleSAML\XML\ExtendableAttributesTrait;
+use SimpleSAML\XML\ExtendableElementTrait;
+use SimpleSAML\XML\Exception\InvalidDOMElementException;
+use SimpleSAML\XML\Exception\SchemaViolationException;
+
+use function array_unshift;
+
+/**
+ * Class defining the TransformationParametersType element
+ *
+ * @package tvdijen/ws-security
+ */
+abstract class AbstractTransformationParametersType extends AbstractWsseElement
+{
+    use ExtendableAttributesTrait;
+    use ExtendableElementTrait;
+
+    /** The namespace-attribute for the xs:anyAttribute element */
+    public const XS_ANY_ATTR_NAMESPACE = C::XS_ANY_NS_OTHER;
+
+    /** The namespace-attribute for the xs:any element */
+    public const XS_ANY_ELT_NAMESPACE = C::XS_ANY_NS_ANY;
+
+
+    /**
+     * TransformationParametersType constructor
+     *
+     * @param \SimpleSAML\XML\SerializableElementInterface[] $children
+     * @param array $namespacedAttributes
+     */
+    public function __construct(
+        array $children = [],
+        array $namespacedAttributes = []
+    ) {
+        $this->setElements($children);
+        $this->setAttributesNS($namespacedAttributes);
+    }
+
+
+    /**
+     * Test if an object, at the state it's in, would produce an empty XML-element
+     *
+     * @return bool
+     */
+    public function isEmptyElement(): bool
+    {
+        return empty($this->getElements()) && empty($this->getAttributesNS());
+    }
+
+
+    /**
+     * Create an instance of this object from its XML representation.
+     *
+     * @param \DOMElement $xml
+     * @return static
+     *
+     * @throws \SimpleSAML\XML\Exception\InvalidDOMElementException
+     *   if the qualified name of the supplied element is wrong
+     */
+    public static function fromXML(DOMElement $xml): static
+    {
+        Assert::same($xml->localName, static::getLocalName(), InvalidDOMElementException::class);
+        Assert::same($xml->namespaceURI, static::NS, InvalidDOMElementException::class);
+
+        $children = [];
+        foreach ($xml->childNodes as $child) {
+            if (!($child instanceof DOMElement)) {
+                continue;
+            }
+
+            $children[] = new Chunk($child);
+        }
+
+        return new static(
+            $children,
+            self::getAttributesNSFromXML($xml),
+        );
+    }
+
+
+    /**
+     * Add this username token to an XML element.
+     *
+     * @param \DOMElement $parent The element we should append this username token to.
+     * @return \DOMElement
+     */
+    public function toXML(DOMElement $parent = null): DOMElement
+    {
+        $e = parent::instantiateParentElement($parent);
+
+        foreach ($this->getAttributesNS() as $attr) {
+            $attr->toXML($e);
+        }
+
+        /** @psalm-var \SimpleSAML\XML\SerializableElementInterface $child */
+        foreach ($this->getElements() as $child) {
+            if (!$child->isEmptyElement()) {
+                $child->toXML($e);
+            }
+        }
+
+        return $e;
+    }
+}
