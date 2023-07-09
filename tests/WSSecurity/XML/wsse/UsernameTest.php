967bd72d0f8c117ea7a82a2fd36cfc58ed0433f5
Tim van Dijen <tvdijen@gmail.com>
2023-07-09 22:36:04 +0200

    Unit-test wsse:Username

---
+++ tests/WSSecurity/XML/wsse/UsernameTest.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\Test\WSSecurity\XML\wsse;
+
+use DOMDocument;
+use PHPUnit\Framework\TestCase;
+use SimpleSAML\WSSecurity\Constants as C;
+use SimpleSAML\WSSecurity\XML\wsse\Username;
+use SimpleSAML\XML\Attribute as XMLAttribute;
+use SimpleSAML\XML\DOMDocumentFactory;
+use SimpleSAML\XML\TestUtils\SerializableElementTestTrait;
+
+use function dirname;
+use function strval;
+
+/**
+ * Tests for wsse:Username.
+ *
+ * @covers \SimpleSAML\WSSecurity\XML\wsse\Username
+ * @covers \SimpleSAML\WSSecurity\XML\wsse\AbstractAttributedString
+ * @covers \SimpleSAML\WSSecurity\XML\wsse\AbstractWsseElement
+ * @package tvdijen/ws-security
+ */
+final class UsernameTest extends TestCase
+{
+    use SerializableElementTestTrait;
+
+
+    /**
+     */
+    public static function setUpBeforeClass(): void
+    {
+        self::$testedClass = Username::class;
+
+        self::$xmlRepresentation = DOMDocumentFactory::fromFile(
+            dirname(__FILE__, 4) . '/resources/xml/wsse_Username.xml',
+        );
+    }
+
+
+    // test marshalling
+
+
+    /**
+     * Test creating a Username object from scratch.
+     */
+    public function testMarshalling(): void
+    {
+        $attr = new XMLAttribute('urn:x-simplesamlphp:namespace', 'ssp', 'attr1', 'testval1');
+        $username = new Username('johndoe@example.com', 'SomeID', [$attr]);
+
+        $this->assertEquals(
+            self::$xmlRepresentation->saveXML(self::$xmlRepresentation->documentElement),
+            strval($username),
+        );
+    }
+
+
+    // test unmarshalling
+
+
+    /**
+     * Test creating a Username from XML.
+     */
+    public function testUnmarshalling(): void
+    {
+        $username = Username::fromXML(self::$xmlRepresentation->documentElement);
+        $this->assertEquals(
+            self::$xmlRepresentation->saveXML(self::$xmlRepresentation->documentElement),
+            strval($username),
+        );
+    }
+}
