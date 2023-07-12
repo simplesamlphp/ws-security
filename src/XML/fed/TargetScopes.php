5b03e662c65ea2980299f3efe8354a803cd90b32
Tim van Dijen <tvdijen@gmail.com>
2023-07-12 20:40:46 +0200

    Add fed:TargetScopes

---
+++ src/XML/fed/TargetScopes.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\WSSecurity\XML\fed;
+
+/**
+ * Class defining the TargetScopes element
+ *
+ * @package tvdijen/ws-security
+ */
+final class TargetScopes extends AbstractEndpointType
+{
+}
