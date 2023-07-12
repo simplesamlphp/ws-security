cbe1790b71d6616a6be2a343ed5e2c58d75b4e84
Tim van Dijen <tvdijen@gmail.com>
2023-07-12 20:44:52 +0200

    Add fed:SecurityTokenServiceEndpoint

---
+++ src/XML/fed/SecurityTokenServiceEndpoint.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\WSSecurity\XML\fed;
+
+/**
+ * Class defining the SecurityTokenServiceEndpoint element
+ *
+ * @package tvdijen/ws-security
+ */
+final class SecurityTokenServiceEndpoint extends AbstractEndpointType
+{
+}
