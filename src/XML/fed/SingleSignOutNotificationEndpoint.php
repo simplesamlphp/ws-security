2d112699f18f2fa40634beb6958c27380f2eb4bb
Tim van Dijen <tvdijen@gmail.com>
2023-07-13 21:12:30 +0200

    Add fed:SingleSignOutNotificationEndpoint

---
+++ src/XML/fed/SingleSignOutNotificationEndpoint.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\WSSecurity\XML\fed;
+
+/**
+ * Class defining the SingleSignOutNotificationEndpoint element
+ *
+ * @package tvdijen/ws-security
+ */
+final class SingleSignOutNotificationEndpoint extends AbstractEndpointType
+{
+}
