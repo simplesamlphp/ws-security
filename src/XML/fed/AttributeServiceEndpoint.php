643685f14985eb5629719f85351cf0ba3cc65b00
Tim van Dijen <tvdijen@gmail.com>
2023-07-13 23:16:42 +0200

    Add fed:AttributeServiceEndpoint

---
+++ src/XML/fed/AttributeServiceEndpoint.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\WSSecurity\XML\fed;
+
+/**
+ * Class defining the AttributeServiceEndpoint element
+ *
+ * @package tvdijen/ws-security
+ */
+final class AttributeServiceEndpoint extends AbstractEndpointType
+{
+}
