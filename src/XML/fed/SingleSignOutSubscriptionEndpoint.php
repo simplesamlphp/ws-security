49e7f6a72c01998866dca88cc315c183e28fa951
Tim van Dijen <tvdijen@gmail.com>
2023-07-13 21:08:17 +0200

    Add fed:SingleSignOutSubscriptionEndpoint

---
+++ src/XML/fed/SingleSignOutSubscriptionEndpoint.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\WSSecurity\XML\fed;
+
+/**
+ * Class defining the SingleSignOutSubscriptionEndpoint element
+ *
+ * @package tvdijen/ws-security
+ */
+final class SingleSignOutSubscriptionEndpoint extends AbstractEndpointType
+{
+}
