d50299e78e75929cdc9fc79e429747017badca67
Tim van Dijen <tvdijen@gmail.com>
2023-07-15 22:08:18 +0200

    Add auth:Description

---
+++ src/XML/auth/Description.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\WSSecurity\XML\auth;
+
+/**
+ * Class representing WS-authorization Description.
+ *
+ * @package tvdijen/ws-security
+ */
+final class Description extends AbstractDescriptionType
+{
+}
---
+++ tests/WSSecurity/XML/auth/DescriptionTest.php
@@ -0,0 +1,76 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\Test\WSSecurity\XML\auth;
+
+use DOMDocument;
+use PHPUnit\Framework\TestCase;
+use SimpleSAML\Assert\AssertionFailedException;
+use SimpleSAML\WSSecurity\Constants;
+use SimpleSAML\WSSecurity\XML\auth\Description;
+use SimpleSAML\XML\Attribute;
+use SimpleSAML\XML\DOMDocumentFactory;
+use SimpleSAML\XML\TestUtils\SerializableElementTestTrait;
+
+use function dirname;
+use function strval;
+
+/**
+ * Tests for auth:Description.
+ *
+ * @covers \SimpleSAML\WSSecurity\XML\auth\Description
+ * @covers \SimpleSAML\WSSecurity\XML\auth\AbstractDescriptionType
+ * @covers \SimpleSAML\WSSecurity\XML\auth\AbstractAuthElement
+ * @package tvdijen/ws-security
+ */
+final class DescriptionTest extends TestCase
+{
+    use SerializableElementTestTrait;
+
+
+    /**
+     */
+    public static function setUpBeforeClass(): void
+    {
+        self::$testedClass = Description::class;
+
+        self::$xmlRepresentation = DOMDocumentFactory::fromFile(
+            dirname(__FILE__, 4) . '/resources/xml/auth_Description.xml'
+        );
+    }
+
+
+    // test marshalling
+
+
+    /**
+     * Test creating an Description object from scratch.
+     */
+    public function testMarshalling(): void
+    {
+        $attr1 = new Attribute('urn:x-simplesamlphp:namespace', 'ssp', 'attr1', 'testval1');
+        $description = new Description('MyDescription', [$attr1]);
+
+        $this->assertEquals(
+            self::$xmlRepresentation->saveXML(self::$xmlRepresentation->documentElement),
+            strval($description)
+        );
+    }
+
+
+    // test unmarshalling
+
+
+    /**
+     * Test creating a Description from XML.
+     */
+    public function testUnmarshalling(): void
+    {
+        $description = Description::fromXML(self::$xmlRepresentation->documentElement);
+        $this->assertEquals(
+            self::$xmlRepresentation->saveXML(self::$xmlRepresentation->documentElement),
+            strval($description)
+        );
+    }
+}
