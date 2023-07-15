6992d412724fc1cf885cc9940fdfeab087c74cc9
Tim van Dijen <tvdijen@gmail.com>
2023-07-15 23:53:36 +0200

    Add auth:StructuredValue

---
+++ src/XML/auth/AbstractStructuredValueType.php
@@ -0,0 +1,104 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\WSSecurity\XML\auth;
+
+use DOMElement;
+use SimpleSAML\Assert\Assert;
+use SimpleSAML\WSSecurity\Constants as C;
+use SimpleSAML\XML\Chunk;
+use SimpleSAML\XML\ExtendableAttributesTrait;
+use SimpleSAML\XML\ExtendableElementTrait;
+use SimpleSAML\XML\Exception\InvalidDOMElementException;
+use SimpleSAML\XML\Exception\SchemaViolationException;
+
+/**
+ * Class defining the StructuredValueType element
+ *
+ * @package tvdijen/ws-security
+ */
+abstract class AbstractStructuredValueType extends AbstractAuthElement
+{
+    use ExtendableAttributesTrait;
+    use ExtendableElementTrait;
+
+    /** The namespace-attribute for the xs:anyAttribute element */
+    public const XS_ANY_ATTR_NAMESPACE = C::XS_ANY_NS_OTHER;
+
+    /** The namespace-attribute for the xs:any element */
+    public const XS_ANY_ELT_NAMESPACE = C::XS_ANY_NS_OTHER;
+
+
+    /**
+     * AbstractStructuredValueType constructor
+     *
+     * @param \SimpleSAML\XML\SerializableElementInterface[] $children
+     * @param array $namespacedAttributes
+     */
+    public function __construct(
+        array $children = [],
+        array $namespacedAttributes = []
+    ) {
+        // Next one is debatable since the schema allows an empty element, but that makes zero sense
+        Assert::allNotEmpty([$children, $namespacedAttributes]);
+
+        $this->setElements($children);
+        $this->setAttributesNS($namespacedAttributes);
+    }
+
+
+    /**
+     * Create an instance of this object from its XML representation.
+     *
+     * @param \DOMElement $xml
+     * @return static
+     *
+     * @throws \SimpleSAML\XML\Exception\InvalidDOMElementException
+     *   if the qualified name of the supplied element is wrong
+     */
+    public static function fromXML(DOMElement $xml): static
+    {
+        Assert::same($xml->localName, static::getLocalName(), InvalidDOMElementException::class);
+        Assert::same($xml->namespaceURI, static::NS, InvalidDOMElementException::class);
+
+        $children = [];
+        foreach ($xml->childNodes as $child) {
+            if (!($child instanceof DOMElement)) {
+                continue;
+            }
+
+            $children[] = new Chunk($child);
+        }
+
+        return new static(
+            $children,
+            self::getAttributesNSFromXML($xml),
+        );
+    }
+
+
+    /**
+     * Add this StructuredValueType to an XML element.
+     *
+     * @param \DOMElement $parent The element we should append this username token to.
+     * @return \DOMElement
+     */
+    public function toXML(DOMElement $parent = null): DOMElement
+    {
+        $e = parent::instantiateParentElement($parent);
+
+        foreach ($this->getAttributesNS() as $attr) {
+            $attr->toXML($e);
+        }
+
+        /** @psalm-var \SimpleSAML\XML\SerializableElementInterface $child */
+        foreach ($this->getElements() as $child) {
+            if (!$child->isEmptyElement()) {
+                $child->toXML($e);
+            }
+        }
+
+        return $e;
+    }
+}
