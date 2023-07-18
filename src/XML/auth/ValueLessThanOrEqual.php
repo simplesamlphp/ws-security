676dc0fdacc06a54455e0f0af0e97845c6b45ab3
Tim van Dijen <tvdijen@gmail.com>
2023-07-18 22:15:34 +0200

    Add auth:ValueLessThanOrEqual

---
+++ src/XML/auth/ValueLessThanOrEqual.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\WSSecurity\XML\auth;
+
+/**
+ * Class representing WS-authorization ValueLessThanOrEqual.
+ *
+ * @package tvdijen/ws-security
+ */
+final class ValueLessThanOrEqual extends AbstractConstrainedSingleValueType
+{
+}
--- tests/WSSecurity/XML/auth/ConstrainedSingleValueTest.php
+++ tests/WSSecurity/XML/auth/ConstrainedSingleValueTest.php
@@ -76,7 +76,7 @@ final class ConstrainedSingleValueTest extends TestCase
             'auth:ValueUpperBound' => [ValueUpperBound::class],
             'auth:ValueLowerBound' => [ValueLowerBound::class],
             'auth:ValueLessThan' => [ValueLessThan::class],
-//            'auth:ValueLessThanOrEqual' => [ValueLessThanOrEqual::class],
+            'auth:ValueLessThanOrEqual' => [ValueLessThanOrEqual::class],
 //            'auth:ValueGreaterThan' => [ValueGreaterThan::class],
 //            'auth:ValueGreaterThanOrEqual' => [ValueGreaterThanOrEqual::class],
         ];
