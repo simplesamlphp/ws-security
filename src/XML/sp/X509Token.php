7a594338c550814b0485634d99e59954056f958f
Tim van Dijen <tvdijen@gmail.com>
2023-12-30 00:54:38 +0100

    Add securitypolicy classes

---
+++ src/XML/sp/X509Token.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\WSSecurity\XML\sp;
+
+use SimpleSAML\Assert\Assert;
+
+/**
+ * An X509Token element
+ *
+ * @package tvdijen/ws-security
+ */
+final class X509Token extends AbstractTokenAssertionType
+{
+}
