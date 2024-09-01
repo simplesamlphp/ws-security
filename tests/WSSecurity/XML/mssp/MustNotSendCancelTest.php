2d2f3ae582935864e2ca5008474715dc0f8d0c53
Tim van Dijen <tvdijen@gmail.com>
2024-09-02 00:47:21 +0200

    Add mssp-elements

---
+++ tests/WSSecurity/XML/mssp/MustNotSendCancelTest.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\Test\WSSecurity\XML\mssp;
+
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\TestCase;
+use SimpleSAML\Test\WSSecurity\XML\sp\QNameAssertionTypeTestTrait;
+use SimpleSAML\WSSecurity\XML\mssp\MustNotSendCancel;
+use SimpleSAML\WSSecurity\XML\sp\AbstractQNameAssertionType;
+use SimpleSAML\WSSecurity\XML\sp\AbstractSpElement;
+use SimpleSAML\XML\DOMDocumentFactory;
+use SimpleSAML\XML\TestUtils\SchemaValidationTestTrait;
+use SimpleSAML\XML\TestUtils\SerializableElementTestTrait;
+
+use function dirname;
+
+/**
+ * Class \SimpleSAML\WSSecurity\XML\sp\MustNotSendCancelTest
+ *
+ * @package simplesamlphp/ws-security
+ */
+#[Group('sp')]
+#[CoversClass(MustNotSendCancel::class)]
+#[CoversClass(AbstractQNameAssertionType::class)]
+#[CoversClass(AbstractSpElement::class)]
+final class MustNotSendCancelTest extends TestCase
+{
+    use QNameAssertionTypeTestTrait;
+    use SchemaValidationTestTrait;
+    use SerializableElementTestTrait;
+
+
+    /**
+     */
+    public static function setUpBeforeClass(): void
+    {
+        self::$schemaFile = dirname(__FILE__, 5) . '/resources/schemas/mssp.xsd';
+
+        self::$testedClass = MustNotSendCancel::class;
+
+        self::$xmlRepresentation = DOMDocumentFactory::fromFile(
+            dirname(__FILE__, 4) . '/resources/xml/mssp_MustNotSendCancel.xml',
+        );
+    }
+}
---
+++ tests/WSSecurity/XML/mssp/SslContextTokenTest.php
@@ -0,0 +1,87 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\Test\WSSecurity\XML\mssp;
+
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\TestCase;
+use SimpleSAML\Test\WSSecurity\Constants as C;
+use SimpleSAML\WSSecurity\XML\mssp\SslContextToken;
+use SimpleSAML\WSSecurity\XML\sp\AbstractTokenAssertionType;
+use SimpleSAML\WSSecurity\XML\sp\IncludeToken;
+use SimpleSAML\WSSecurity\XML\sp\IncludeTokenTypeTrait;
+use SimpleSAML\XML\Attribute as XMLAttribute;
+use SimpleSAML\XML\Chunk;
+use SimpleSAML\XML\DOMDocumentFactory;
+use SimpleSAML\XML\TestUtils\SchemaValidationTestTrait;
+use SimpleSAML\XML\TestUtils\SerializableElementTestTrait;
+
+use function dirname;
+
+/**
+ * Class \SimpleSAML\WSSecurity\XML\mssp\SslContextTokenTest
+ *
+ * @package simplesamlphp/ws-security
+ */
+#[Group('mssp')]
+#[CoversClass(SslContextToken::class)]
+#[CoversClass(IncludeTokenTypeTrait::class)]
+#[CoversClass(AbstractTokenAssertionType::class)]
+#[CoversClass(AbstractSpElement::class)]
+final class SslContextTokenTest extends TestCase
+{
+    use SchemaValidationTestTrait;
+    use SerializableElementTestTrait;
+
+
+    /**
+     */
+    public static function setUpBeforeClass(): void
+    {
+        self::$schemaFile = dirname(__FILE__, 5) . '/resources/schemas/mssp.xsd';
+
+        self::$testedClass = SslContextToken::class;
+
+        self::$xmlRepresentation = DOMDocumentFactory::fromFile(
+            dirname(__FILE__, 4) . '/resources/xml/mssp_SslContextToken.xml',
+        );
+    }
+
+
+    // test marshalling
+
+
+    /**
+     * Adding an empty SslContextToken element should yield an empty element.
+     */
+    public function testMarshallingEmptyElement(): void
+    {
+        $msspns = C::NS_WS_SEC;
+        $sslContextToken = new SslContextToken();
+        $this->assertEquals(
+            "<mssp:SslContextToken xmlns:mssp=\"$msspns\"/>",
+            strval($sslContextToken),
+        );
+        $this->assertTrue($sslContextToken->isEmptyElement());
+    }
+
+
+    /**
+     * Test that creating a SslContextToken from scratch works.
+     */
+    public function testMarshalling(): void
+    {
+        $attr = new XMLAttribute(C::NAMESPACE, 'ssp', 'attr1', 'value1');
+        $chunk = new Chunk(DOMDocumentFactory::fromString(
+            '<ssp:Chunk xmlns:ssp="urn:x-simplesamlphp:namespace">some</ssp:Chunk>',
+        )->documentElement);
+
+        $sslContextToken = new SslContextToken(IncludeToken::Always, [$chunk], [$attr]);
+        $this->assertEquals(
+            self::$xmlRepresentation->saveXML(self::$xmlRepresentation->documentElement),
+            strval($sslContextToken),
+        );
+    }
+}
